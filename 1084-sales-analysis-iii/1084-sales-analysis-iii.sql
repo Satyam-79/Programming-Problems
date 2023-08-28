b7c5dc606cd3b17b2389a3e808e2e2b90edff153
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-08-28 10:08:58 +0530

    Time: 2773 ms (13.38%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,17 +1,4 @@
 # Write your MySQL query statement below
-# SELECT
-# 	s.product_id   AS product_id,
-# 	p.product_name AS product_name
-# FROM
-# 	Sales s
-# 	INNER JOIN
-# 		Product p
-# 		ON
-# 			s.product_id=p.product_id
-# WHERE
-# 	s.sale_date BETWEEN '2019-01-01' and '2019-03-31'
-#     AND p.product_id NOT IN (SELECT ss.product_id FROM Sales ss 
-#           WHERE ss.sale_date NOT BETWEEN '2019-01-01' AND '2019-03-31')
 
 SELECT
 	s.product_id   AS product_id,
