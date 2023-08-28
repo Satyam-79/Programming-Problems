18606d45bb96f97dc2c132ec2b3437ddf0d163c8
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-08-28 09:18:22 +0530

    Time: 2754 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -4,11 +4,13 @@ SELECT
 	e.name               AS name         ,
 	COUNT(e.employee_id) AS reports_count,
 	ROUND(AVG(m.age))    AS average_age
-    # e.age as age
 FROM
-	Employees e,
-	Employees m
-WHERE
-	e.employee_id=m.reports_to
-GROUP BY employee_id
-ORDER BY employee_id
+	Employees e
+	INNER JOIN
+		Employees m
+		ON
+			e.employee_id=m.reports_to
+GROUP BY
+	employee_id
+ORDER BY
+	employee_id
