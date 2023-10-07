3c3bc4b55a6fd4d4b23783a88ccb9020a4369854
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-10-07 16:40:37 +0530

    Time: 1095 ms (37.69%), Space: 159.2 MB (25.00%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
@@ -1,17 +1,33 @@
 class Solution:
-    def minSwap(self, A: List[int], B: List[int]) -> int:
-        @cache
-        def rec(idx,lastA,lastB):
-            if idx==len(A):
+    def minSwap(self, nums1, nums2):
+        n = len(nums1)
+        dp = [-1] * n
+
+        def solve(ind):
+            if ind == n:
                 return 0
-            a,b=float('inf'),float('inf')
-            if A[idx]>lastA and B[idx]>lastB:
-                a=rec(idx+1,A[idx],B[idx])
-                
-            if A[idx]>lastB and B[idx]>lastA:
-                b=1+rec(idx+1,B[idx],A[idx])
-                
-            return min(a,b)
-        
-        return rec(0,-1,-1)
+
+            if ind > 0 and (nums1[ind-1] >= nums1[ind] or nums2[ind-1] >= nums2[ind]):
+                nums1[ind], nums2[ind] = nums2[ind], nums1[ind]
+                val = 1 + solve(ind + 1)
+                nums1[ind], nums2[ind] = nums2[ind], nums1[ind]  # Swap back
+                return val
+
+            elif ind > 0 and (nums1[ind-1] >= nums2[ind] or nums2[ind-1] >= nums1[ind]):
+                return solve(ind + 1)
+
+            else:
+                if dp[ind] != -1:
+                    return dp[ind]
+
+                tempAns1 = solve(ind + 1)
+
+                nums1[ind], nums2[ind] = nums2[ind], nums1[ind]
+                tempAns2 = 1 + solve(ind + 1)
+                nums1[ind], nums2[ind] = nums2[ind], nums1[ind]  # Swap back
+
+                dp[ind] = min(tempAns1, tempAns2)
+                return dp[ind]
+
+        return solve(0)
                 
