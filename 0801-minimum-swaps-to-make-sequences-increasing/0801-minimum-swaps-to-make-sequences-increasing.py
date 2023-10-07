c630d6cc593015b91d8f522259b564d94a991d68
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-10-07 16:38:19 +0530

    Time: 1409 ms (6.16%), Space: 304.3 MB (5.39%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.py
@@ -1,35 +1,17 @@
 class Solution:
     def minSwap(self, A: List[int], B: List[int]) -> int:
-        
-        memo = {}
-        def dp(index, lastA, lastB) -> int:
-            # returns the minimum swaps necessary
-            # to ensure the array only increases
-            # after this index
-            if index == len(A):
+        @cache
+        def rec(idx,lastA,lastB):
+            if idx==len(A):
                 return 0
-            
-            key = str(index)+','+str(lastA)+','+str(lastB)
-            if key in memo:
-                return memo[key]
-            
-            
-            # can continue without swapping
-            if A[index]>lastA and B[index]>lastB:
-                a = dp(index+1, A[index], B[index])
-            else:
-                a = float("inf")
-            
-            # can swap and continue
-            if B[index]>lastA and A[index]>lastB:
-                b = 1 + dp(index+1, B[index], A[index])
-            else:
-                b = float("inf")
-            
-            
-            memo[key] = min(a,b)
-            return memo[key]
+            a,b=float('inf'),float('inf')
+            if A[idx]>lastA and B[idx]>lastB:
+                a=rec(idx+1,A[idx],B[idx])
                 
+            if A[idx]>lastB and B[idx]>lastA:
+                b=1+rec(idx+1,B[idx],A[idx])
+                
+            return min(a,b)
         
-        return dp(0,-1,-1)
+        return rec(0,-1,-1)
                 
