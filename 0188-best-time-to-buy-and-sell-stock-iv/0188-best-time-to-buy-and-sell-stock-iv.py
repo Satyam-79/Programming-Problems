fb4c61f0343f72dfb28f0b7ddeaef89ba9882ead
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-10-13 10:47:55 +0530

    Time: 270 ms (25.46%), Space: 91.8 MB (5.03%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        @cache
+        def rec(idx,buy,depth):
+            if idx>=len(prices) or depth==0:
+                return 0
+            if buy:
+                return max(
+                    -prices[idx]+rec(idx+1,0,depth),
+                    rec(idx+1,1,depth)
+                )
+            return max(
+                prices[idx]+rec(idx+1,1,depth-1),
+                rec(idx+1,0,depth)
+            )
+        
+        return rec(0,1,k)
