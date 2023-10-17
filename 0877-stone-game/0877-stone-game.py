77d559b089c7cb3fd291d880b9c3d63cd948d5bf
Satyam <70771653+Satyam-79@users.noreply.github.com>
2023-10-17 12:46:52 +0530

    Time: 402 ms (36.52%), Space: 128.4 MB (6.53%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,6 +1,13 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
-        # @cache
-        # def rec(i,stones):
-        #     if i
+        
+        @cache
+        def rec(i,j):
+            if i >= j:
+                return False
+            return max(
+                piles[i] +rec(i+1,j),
+                piles[j]+rec(i,j-1)
+            )
+        
+        return rec(0,len(piles)-1)
